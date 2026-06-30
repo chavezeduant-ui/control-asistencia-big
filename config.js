5cd4144894137537becf5fa2fbd403c194702be2
chavezeduant-ui <chavezeduant@gmail.com>
2026-06-30 12:44:25 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuración Firebase del proyecto Control Asistencia BIG
+// Puedes reemplazar estos datos si usas otro proyecto.
+window.SIGE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAuIl2pDUm90SjGWAhGXS8Z4kpxWBGOzgE",
+  authDomain: "control-asistencia-big.firebaseapp.com",
+  projectId: "control-asistencia-big",
+  storageBucket: "control-asistencia-big.firebasestorage.app",
+  messagingSenderId: "580360757102",
+  appId: "1:580360757102:web:16c65b415f195498bc2b7f",
+  measurementId: "G-BH33BF7TRC"
+};
